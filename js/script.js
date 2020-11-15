6e0f6c8e8d053e6384b19ed8a563577bc1670a2e
Zukha <zukhriddin853@gmail.com>
2020-11-15 22:27:09 +0000

    first scripts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function 123(wqe) {
-	asd
-}
+const numberOfFilms = prompt("How many movies have you watched already?", "");
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false
+};
+
+const a = prompt("The last wathced movie?", ""),
+						b = prompt("How good was it?", ""),
+						c = prompt("The last wathced movie?", ""),
+						d = prompt("How good was it?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
