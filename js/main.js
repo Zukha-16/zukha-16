83b0b8a952e91c7a35fb6ad60f1228c65c8baf3b
Zukha <zukhriddin853@gmail.com>
2020-11-15 21:16:03 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = +prompt("Are you 18?", "18");
+console.log(answer + 10);
+
+if (answer !== 18) {
+	alert("Sorry")
+} else {
+	alert("Welcome")
+} */
+
+/* const answers = [];
+answers[0] = prompt("What is your firstname?", "");
+answers[1] = prompt("What is your lastname?", "");
+answers[2] = prompt("What is your age?", "");
+
+document.write(answers) */
+
+/* const category = 'toys';
+console.log(`http://someurl.com/${category}/5` );
+
+const user = 'Zukha';
+alert(`Hello ${user}`) */
+
+const isChecked = true,
+						isClosed  = false;
+
+console.log(isChecked || isClosed);
