d9f18724ebb6cfe6c7ddb7a5592417480529d945
Zukha <zukhriddin853@gmail.com>
2020-11-17 04:40:18 +0000

    new java functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("How many movies have you watched already?", "");
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt("How many movies have you watched already?", "");
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt("How many movies have you watched already?", "");
+	}
+}
+
+start();
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -8,29 +18,52 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < numberOfFilms; i++){
-const a = prompt("The last wathced movie?", ""),
-						b = prompt("How good was it?", "");
 
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt("The last wathced movie?", ""),
+			b = prompt("How good was it?", "");
+
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 			personalMovieDB.movies[a] = b;
 			console.log('done');
-		} else{
+		} else {
 			console.log('error');
 			i--;
 		}
-		
+	}
 }
-if (personalMovieDB.count == 0) {
-	alert("You don't like movies?")
-} else if (personalMovieDB.count < 10 && personalMovieDB.count > 0){
-	alert('Not so many movies watched!')
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
-	alert('Not bad! You are classic movie watcher!')
-} else if (personalMovieDB.count > 30) {
-	alert('You are big fan of movies!')	
-} else{
-	alert('Error!')
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count == 0) {
+		alert("You don't like movies?")
+	} else if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+		alert('Not so many movies watched!')
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+		alert('Not bad! You are classic movie watcher!')
+	} else if (personalMovieDB.count > 30) {
+		alert('You are big fan of movies!')
+	} else {
+		alert('Error!')
+	}
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
 }
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt("What is your " + i + " - favourite genre of music")
+	}
+}
+writeYourGenres()
+
+
+showMyDB(personalMovieDB.private)
